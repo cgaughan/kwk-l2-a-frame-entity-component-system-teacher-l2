f81886815ba8a94fbba61d50e1222888e5948fe4
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-26 03:10:14 +0000

    Done.

--- index.js
+++ index.js
@@ -40,5 +40,26 @@ function createSphere() {
 }
 
 function createSpheres() {
-  // TODO: implement me!
+  x = 4
+  while (x > 0) {
+    setTimeout(() => {
+      let [el, coords] = createSphere()
+      el = addBobAnimationToElement(el, coords)
+      addEntityToScene(el)
+    }, Math.random() * 2500)
+    x--
+  }
+}
+
+function addBobAnimationToElement(el, coord) {
+  const newAnim = document.createElement('a-animation')
+  newAnim.setAttribute("attribute", "position")
+  newAnim.setAttribute("repeat", "indefinite")
+  newAnim.setAttribute("from", [coord[0], coord[1] - 1, coord[2]].join(" "))
+  newAnim.setAttribute("to", [coord[0], coord[1] + 1, coord[2]].join(" "))
+  newAnim.setAttribute("easing", "ease")
+  newAnim.setAttribute("direction", "alternate")
+  newAnim.setAttribute("dur", "2000")
+  el.appendChild(newAnim)
+  return el
 }
